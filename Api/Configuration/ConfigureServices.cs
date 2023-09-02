04a187ac7a30edb37f9f1e116f59900e04df0124
Felipe Marciano <felipebsq@gmail.com>
2023-09-02 02:51:37 -0300

    .

--- Api/Configuration/ConfigureServices.cs
+++ Api/Configuration/ConfigureServices.cs
@@ -18,7 +18,7 @@ namespace Api.Configuration
 
             if (Environment.GetEnvironmentVariable("HOST") != null)
             {
-                defaultConnectionString = $"Host=Environment.GetEnvironmentVariable(\"Host\"); Database=Environment.GetEnvironmentVariable(\"Database\") Username=Environment.GetEnvironmentVariable(\"Username\"); Password=Environment.GetEnvironmentVariable(\"Password\")";
+                defaultConnectionString = $"Host={Environment.GetEnvironmentVariable("Host")}; Database={Environment.GetEnvironmentVariable("Database")} Username={Environment.GetEnvironmentVariable("Username")}; Password={Environment.GetEnvironmentVariable("Password")}";
             }
 
             Infrastructure.Dependencies.ConfigureServices(configuration, services, defaultConnectionString, defaultConnectionString);
